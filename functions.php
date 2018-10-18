ca3d03b652ede2220716c860b3fd3b4f2249ad5e
GallaxyGS <vas@email.com>
2018-10-18 15:28:53 +0200

    hh

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php function obsahkruhu($r)
+{
+	$obsahkruhu  = 3.14*$r*$r;
+	return $obsahkruhu;
+}
+
+function obvodkruhu($r)
+{
+  $obvodkruhu = 2*3.14*$r;
+  return $obvodkruhu;
+}
+?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include_once "functions.php";
+$obsahkruhu = obsahkruhu(5);
+$obvodkruhu = obvodkruhu(5);
+echo $obvodkruhu , "</br>";
+echo $obsahkruhu;
+?>
